267ee947cb397e65475a512cb517f7238f7c307c
QiaoBaoJun <chnqbj@gmail.com>
2016-12-15 23:23:44 +0800

    vue-resource header 组件初步

--- workspace/src/components/header/header.vue
+++ workspace/src/components/header/header.vue
@@ -1,10 +1,35 @@
 <template lang="html">
     <div class="header">
-        i am header....
+        <div class="content-wrapper">
+            <div class="avatar">
+                <img width="64" height="64" :src="seller.avatar">
+            </div>
+            <div class="content">
+                <div class="title">
+                    <span class="brand"></span>
+                    <span class="name">{{ seller.name }}</span>
+                </div>
+            </div>
+            <div class="description">
+                {{ seller.description }} / {{ seller.deliveryTime + '分钟送达'}}
+            </div>
+            <div v-if="seller.supports" class="support">
+                <span class="icon"></span>
+                <span class="text"> {{ seller.supports[0].description}}</span>
+            </div>
+        </div>
+        <div class="bulletin-wrapper">
+
+        </div>
     </div>
 </template>
 <script>
 export default {
+    props: {
+        seller: {
+            type: Object
+        }
+    }
 }
 </script>
 
